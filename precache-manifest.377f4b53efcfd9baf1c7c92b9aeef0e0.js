e1510beea7585abd0b889611301c5667e6a32ecb
sahil thakur <sahilthakurxyz@gmail.com>
2023-03-29 14:20:49 +0530

    Updates

---
+++ precache-manifest.377f4b53efcfd9baf1c7c92b9aeef0e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "368b4f038a87c1e6bdbc",
+    "url": "/Amazon-org/static/css/main.cfefcfdc.chunk.css"
+  },
+  {
+    "revision": "368b4f038a87c1e6bdbc",
+    "url": "/Amazon-org/static/js/main.368b4f03.chunk.js"
+  },
+  {
+    "revision": "5d075441c8c23f7c9d08",
+    "url": "/Amazon-org/static/js/1.5d075441.chunk.js"
+  },
+  {
+    "revision": "7ac05292b92387170c97",
+    "url": "/Amazon-org/static/js/2.7ac05292.chunk.js"
+  },
+  {
+    "revision": "e5a73b5c6fdedec022d1",
+    "url": "/Amazon-org/static/js/runtime~main.e5a73b5c.js"
+  },
+  {
+    "revision": "36e08879517b01593573c2a1d9669ad8",
+    "url": "/Amazon-org/index.html"
+  }
+];
